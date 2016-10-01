dfb5db43a26c40d34433402b2eddd8c88c064e58
nycarts <jdalgety@gmail.com>
2016-09-30 20:12:13 -0500

    Updated Algos

---
+++ JennaDalgety/book_challenges1-1.js
@@ -0,0 +1,163 @@
+// function setSwap(myNumber, myName){
+  
+//   var temp1 = myName;
+//   var temp2 = myNumber;
+
+//   myNumber = temp1;
+
+//   console.log(myNumber, myName);
+
+// }
+
+// setSwap(42, 'Jenna');
+
+
+
+// for(var i = -52; i <= 1066; i++){
+//   console.log(i);
+// }
+
+
+
+// function beCheerful(){
+//   for(var i = 1; i <= 98; i++){
+//     console.log('good morning!');
+//   }
+// }
+
+// beCheerful();
+
+
+
+// for(var i = -300; i <= 0; i++){
+//   if(i % 3 === 0){
+//     if(i === -3){
+//       continue;
+//     }
+//     if(i === -6){
+//       continue;
+//     }
+//     console.log(i);
+//   }
+// }
+
+
+
+// var i = 2000
+
+// while(i <= 5280){
+//   console.log(i);
+//   i++;
+// }
+
+
+
+// function yourBirthday(arr){
+
+//   if(arr[0] === 4 && arr[1] === 3){
+//     console.log('How did you know?')
+//   }
+//   else if(arr[0] === 3 && arr[1] === 4){
+//     console.log('How did you know?')
+//   }
+//   else{
+//     console.log('Just another day...')
+//   }
+
+// }
+
+// yourBirthday([6, 1]);
+
+
+
+// function leapYear(year){
+//   if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)){
+//     console.log(year + ' is a leap year.');
+//   }
+//   else{
+//     console.log(year + ' is not a leap year.');
+//   }
+// }
+
+// leapYear(1900);
+
+
+
+// var i = 512;
+// var count = 0;
+
+// while(i <= 4096){
+//   console.log(i);
+//   i++
+//   count++;
+// }
+// console.log(count);
+
+
+
+// var i = 6;
+
+// while(i <= 60000){
+//   if(i % 6 === 0){
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+
+
+// for(var i = 1; i <= 100; i++){
+//   if(i % 5 === 0){
+//     console.log('coding');
+//   }
+//   if(i % 10 === 0){
+//     console.log(' dojo');
+//   }
+//   else{
+//     console.log(i);
+//   }
+// }
+
+
+
+// function what2Know(incoming){
+//   console.log(incoming);
+// }
+
+// what2Know('Everything!');
+
+
+
+// var sum = 0
+
+// for(var i = -300000; i <= 300000; i++){
+//   if(i % 2 !== 0){
+//     sum = sum + i;
+//   }
+// }
+
+// console.log(sum);
+
+
+
+// var i = 2016;
+
+// while(i > 0){
+//   if(i % 4 === 0){
+//     console.log(i);
+//   }
+//   i--;
+// }
+
+
+
+// function flexCoutdown(lowNum, highNum, mult){
+
+//   for(var i = highNum; i > lowNum; i--){
+//     if(i % mult === 0){
+//       console.log(i);
+//     }
+//   }
+// }
+
+// flexCoutdown(2,9,3);
